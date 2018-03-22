62d0d85903c6e1685e09321244c933c04934f1c5
Rohman <a.nurrohman007@gmail.com>
2018-03-22 14:46:37 +0700

    css news

--- src/pages/news1.js
+++ src/pages/news1.js
@@ -84,7 +84,7 @@ class Content extends Component {
 						<div className="title-news"><h1>25 Batang Pipa HDPE Segera Masuk Area Relokasi Bojong Koneng</h1></div>
 						<div className="date-news">07 Oktober 2017</div>
 						<div className="description-news">
-							<p>BOGOR – PDAM Tirta Pakuan Kota Bogor telah menyusun rencana relokasi pipa AC 18“ di area longsor Kampung Bojong Koneng Desa Ciherang Pondok Kecamatan Caringin Kabupaten Bogor.</p>
+							<p>BOGOR – PDAM Tirta Pakuan Kota Bogor telah menyusun rencana relokasi pipa AC 18“ di area longsor Kampung Bojong Koneng Desa Ciherang Pondok</p>
 						</div>
 					</div>
 				</div>
@@ -93,8 +93,10 @@ class Content extends Component {
 				<div className="block-news-detail">
 					<div className="caption-news">
 						<div className="description-news">
+							<p>Kecamatan Caringin Kabupaten Bogor.</p>
 							<p>Sebanyak 25 batang pipa HDPE 18” (sebagai pengganti pipa AC) sudah tersedia dan segera diakut menuju area relokasi.</p>
 							<p>Direktur Teknik PDAM Tirta Pakuan Kota Bogor Syaban Maulana mengatakan pihaknya akan memindahkan pipa AC 18” sepanjang 150 meter dan menggantinya dengan pipa baru jenis HDPE 18”. PDAM telah berkoordinasi dengan pengembang proyek jalan Tol Bocimi untuk memudahkan mobilisasi dan pengangkutan pipa ukuran besar itu.</p>
+							<p>“Alhamdulillah, kita sudah diberi akses</p>
 						</div>
 					</div>
 				</div>
@@ -103,7 +105,7 @@ class Content extends Component {
 				<div className="block-news-detail">
 					<div className="caption-news">
 						<div className="description-news">
-							<p>“Alhamdulillah, kita sudah diberi akses oleh pengembang Bocimi untuk pengiriman truk-truk pengangkut pipa HDPE 18”. Hanya untuk masuk ke area relokasi, pipa tidak bisa masuk karena hanya ada jalan setapak sepanjang 600 meter yang licin saat hujan,” kata Syaban di sela-sela meninjau area relokasi, Jumat (6/10/2017).</p>
+							<p>oleh pengembang Bocimi untuk pengiriman truk-truk pengangkut pipa HDPE 18”. Hanya untuk masuk ke area relokasi, pipa tidak bisa masuk karena hanya ada jalan setapak sepanjang 600 meter yang licin saat hujan,” kata Syaban di sela-sela meninjau area relokasi, Jumat (6/10/2017).</p>
 						</div>
 					</div>
 				</div>
