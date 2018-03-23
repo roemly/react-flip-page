f79e1d34bde2d25cc6ac1a37341943574e8faf1b
Rohman <a.nurrohman007@gmail.com>
2018-03-23 09:31:45 +0700

    data news

--- src/pages/news1.js
+++ src/pages/news1.js
@@ -22,6 +22,9 @@ import imagenews1 from '../assets/martabe.jpg';
 import imagenews2 from '../assets/hdpe.jpg';   
 import imagenews3 from '../assets/bojongkenong.jpg';   
 import imagenews4 from '../assets/news4.jpg';   
+import imagenews5 from '../assets/news5.jpg';   
+import imagenews6 from '../assets/news6.jpg';   
+import imagenews7 from '../assets/news7.jpg';   
 import imagesnews3images from '../assets/news3images.jpg';   
      
 class Content extends Component {
@@ -42,11 +45,24 @@ class Content extends Component {
         <FlipPage
         lastComponent={theEnd}
         >
-            <article>
+            <article className="first_block">
 				<div className="block-news">
 					<div className="list-news">
 					<NavLink tag={RouterNavLink} to="/News/News1/Detail" >
-						<img className="image-news" src={imagenews1} />
+						<div className="image-news-thunmbnail">
+							<img className="image-news" src={imagenews3} />
+						</div>
+						<div className="caption-news">
+							<div className="title-news"><h1>25 Batang Pipa HDPE Segera Masuk Area Relokasi Bojong Koneng</h1></div>
+							<div className="date-news">07 Februari 2018</div>
+						</div>
+					</NavLink>	
+					</div>
+					<div className="list-news">
+					<NavLink tag={RouterNavLink} to="/News/News1/Detail" >
+						<div className="image-news-thunmbnail">
+							<img className="image-news" src={imagenews1} />
+						</div>
 						<div className="caption-news">
 							<div className="title-news"><h1>Proyek pipa HDPE di tambang Emas Martabe</h1></div>
 							<div className="date-news">20 Januari 2018</div>
@@ -55,7 +71,9 @@ class Content extends Component {
 					</div>
 					<div className="list-news">
 						<NavLink tag={RouterNavLink} to="/News/News1/Detail" >
-							<img className="image-news" src={imagenews2} />
+							<div className="image-news-thunmbnail">
+								<img className="image-news" src={imagenews2} />
+							</div>
 							<div className="caption-news">
 								<div className="title-news"><h1>Pipa HDPE Mulai Menggeser PVC</h1></div>
 								<div className="date-news">11 Januari 2018 </div>
@@ -68,19 +86,45 @@ class Content extends Component {
 				<div className="block-news">
 					<div className="list-news">
 					<NavLink tag={RouterNavLink} to="/News/News1/Detail" >
-						<img className="image-news" src={imagenews3} />
+						<div className="image-news-thunmbnail">
+							<img className="image-news" src={imagenews5} />
+						</div>
 						<div className="caption-news">
-							<div className="title-news"><h1>25 Batang Pipa HDPE Segera Masuk Area Relokasi Bojong Koneng</h1></div>
-							<div className="date-news">07 Oktober 2017</div>
+							<div className="title-news"><h1>Pemasangan Pipa HDPE, Solusi Kebocoran Pipa di Tegal Gede</h1></div>
+							<div className="date-news">08 Oktober 2017</div>
 						</div>
 					</NavLink>	
 					</div>
 					<div className="list-news">
 					<NavLink tag={RouterNavLink} to="/News/News1/Detail" >
-						<img className="image-news" src={imagenews4} />
+						<div className="image-news-thunmbnail">
+							<img className="image-news" src={imagenews4} />
+						</div>
 						<div className="caption-news">
 							<div className="title-news"><h1>Pipa PDAM TKR Bocor, Warga Tanah Tinggi Beli Air Galon</h1></div>
-							<div className="date-news">06 Februari 2017</div>
+							<div className="date-news">06 Oktober 2017</div>
+						</div>
+					</NavLink>
+					</div>
+					<div className="list-news">
+					<NavLink tag={RouterNavLink} to="/News/News1/Detail" >
+						<div className="image-news-thunmbnail">
+							<img className="image-news" src={imagenews6} />
+						</div>
+						<div className="caption-news">
+							<div className="title-news"><h1>Pemain Asing Incar Pasar Produk Pipa di Indonesia</h1></div>
+							<div className="date-news">19 November 2017</div>
+						</div>
+					</NavLink>	
+					</div>
+					<div className="list-news">
+					<NavLink tag={RouterNavLink} to="/News/News1/Detail" >
+						<div className="image-news-thunmbnail">
+							<img className="image-news" src={imagenews7} />
+						</div>
+						<div className="caption-news">
+							<div className="title-news"><h1>Vinilon Siap Memperbesar Produksi Pipa</h1></div>
+							<div className="date-news">20 Januari 2018</div>
 						</div>
 					</NavLink>
 					</div>
